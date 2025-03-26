17120f0460949dd61ed9bd0e799dbc18bb1d91f2
GekK9 <110110744+GekK9@users.noreply.github.com>
2025-03-27 03:20:49 +0800

    cleaning the code and  and adding comments

--- PyQT/ex 3.py
+++ PyQT/ex 3.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtWidgets import QPushButton, QLineEdit, QLabel
+from PyQt5.QtWidgets import QPushButton, QLineEdit
 import sys, string
 
 class Window (QWidget):
@@ -29,17 +29,19 @@ class Window (QWidget):
             button.move((i * 25) + 10, 35)
             button.clicked.connect(self.run)
 
+        # create text box
         self.text_box = QLineEdit(self)
         self.text_box.resize(370, 25)
         self.text_box.move(10, 75)
 
-        self.text_box.setText
         self.morse_text = ''
         
+        # set text to text box
     def run(self):
         self.morse_text += (f"{self.to_morse(self.sender().text())} ")
         self.text_box.setText(self.morse_text)
 
+        # traslate letter to morse code
     def to_morse(self, s):
         morse = {
         'a': '.-', 'b': '-...', 'c': '-.-.',

71fd2c7d95ba0480e7e9b00fd3f284f24ea522a7
GekK9 <110110744+GekK9@users.noreply.github.com>
2025-03-27 03:10:59 +0800

    fix

--- PyQT/ex 3.py
+++ PyQT/ex 3.py
@@ -37,7 +37,7 @@ class Window (QWidget):
         self.morse_text = ''
         
     def run(self):
-        self.morse_text += self.to_morse(self.sender().text())
+        self.morse_text += (f"{self.to_morse(self.sender().text())} ")
         self.text_box.setText(self.morse_text)
 
     def to_morse(self, s):
@@ -52,7 +52,7 @@ class Window (QWidget):
         'v': '...-', 'w': '.--', 'x': '-..-',
         'y': '-.--', 'z': '--..',
         }
-        return ' '.join(morse.get(i) for i in s)
+        return morse.get(s)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

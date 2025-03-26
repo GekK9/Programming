d5e17879cb90d13eb76cd49d26c67a5818818f8b
GekK9 <110110744+GekK9@users.noreply.github.com>
2025-03-27 05:01:21 +0800

    Completed ex #4

--- PyQT/ex 4.py
+++ PyQT/ex 4.py
@@ -9,19 +9,30 @@ class Window (QWidget):
 
     def initUI(self):
         # window
-        self.setGeometry(300, 300, 200, 150)
+        self.setGeometry(300, 300, 200, 120)
         self.setWindowTitle('CheckBoxes')
 
-        for i in range(4):
-            check_box = QCheckBox(f"edit{i+1}", self)
-            check_box.move(10, (i * 30) + 20)
+        self.input_boxes = {}
 
         for i in range(4):
-            input_boxes = QLineEdit(self)
-            input_boxes.setText(f"Поле edit{i+1}")
-            input_boxes.move(70, (i * 30) + 17)
-            input_boxes.resize(120, 20)
+            check_box = QCheckBox(f"edit{i + 1}", self)
+            check_box.move(10, 15 + (i * 25))
+            check_box.stateChanged.connect(self.show_and_hide)
 
+            input_box = QLineEdit(f"Поле edit{i + 1}", self)
+            input_box.move(70, (i* 25) + 13)
+            input_box.resize(100, 20)
+
+            self.input_boxes[check_box] = input_box
+
+            input_box.hide()
+            
+    def show_and_hide(self):
+        for check_box, input_box in self.input_boxes.items():
+            if check_box.isChecked():
+                input_box.show()
+            else:
+                input_box.hide()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

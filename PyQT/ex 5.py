1ef420a51fd60a5ad7a2bad435291fde588e7058
GekK9 <110110744+GekK9@users.noreply.github.com>
2025-03-27 18:13:49 +0800

    Completed ex # 5

--- PyQT/ex 5.py
+++ PyQT/ex 5.py
@@ -12,13 +12,15 @@ class Window (QWidget):
         self.setGeometry(300, 300, 300, 360)
         self.setWindowTitle('Order')
 
-        self.main_layout = QHBoxLayout(self)
+        self.main_layout = QVBoxLayout(self)
         self.order_box = QHBoxLayout()
         self.check_boxes = QVBoxLayout()
         self.names = QVBoxLayout()
 
         self.cheque_box_widget = QWidget()
-        self.cheque_box_layout = QVBoxLayout(self.cheque_box_widget) 
+        self.cheque_box_layout = QVBoxLayout(self.cheque_box_widget)
+        self.cheque_box_widget.setStyleSheet("border: 2px solid grey; padding: 10px;")  
+        self.cheque_box_widget.hide()
 
 
         self.main_layout.addLayout(self.order_box)
@@ -26,22 +28,21 @@ class Window (QWidget):
         self.order_box.addLayout(self.check_boxes)
         self.order_box.addLayout(self.names)
         self.main_layout.addStretch()
-        self.cheque_box_layout.minimumHeightForWidth(400)
 
 
 
-        self.goods = [
-            'Чизбургер', 'Гамбургер', 'Кока-кола', 'Нагеттсы',
-            'Картошка Фри', 'Вода', 'Котлета',
-        ]
+        self.goods = {
+            "100" : 'Чизбургер', "80": 'Гамбургер', "60": 'Кока-кола', "120" : 'Нагеттсы',
+            "110": 'Картошка Фри', "40": 'Вода',
+        }
         self.order_box.addStretch()
         self.input_boxes = {
             
         }
         
-        for item in self.goods:
+        for price, name in self.goods.items():
             
-            self.check_box = QCheckBox(f"{item}", self)
+            self.check_box = QCheckBox(f"{name}", self)
             self.check_box.stateChanged.connect(self.count)
             self.check_box.stateChanged.connect(self.order)
             self.check_boxes.addWidget(self.check_box)
@@ -50,7 +51,7 @@ class Window (QWidget):
             input_box.setFixedSize(30, 17)
             input_box.setDisabled(True)
             self.names.addWidget(input_box)
-            self.input_boxes[self.check_box] = input_box
+            self.input_boxes[self.check_box] = (input_box, price)
             
 
         self.check_boxes.setSpacing(15)
@@ -66,32 +67,50 @@ class Window (QWidget):
         self.check_boxes.addWidget(self.btn)
         self.check_boxes.addStretch()
 
-        self.text = QLabel(self)
-        self.text.setText('Ваш заказ:')
         self.goods = QLabel(self)
-        self.cheque_box_layout.addWidget(self.text)
         self.cheque_box_layout.addWidget(self.goods)
         self.goods.setStyleSheet("border: 0px")
-        self.text.setStyleSheet("border: 0px")
-        self.text.hide()
         
     def order(self):
-        self.cheque_box_widget.setStyleSheet("border: 2px solid grey; padding: 10px;") 
-        self.text.show()
-        self.goods.setText('')
+        for i in reversed(range(self.cheque_box_layout.count())): 
+            widget = self.cheque_box_layout.itemAt(i).widget()
+            if widget is not None:
+                widget.deleteLater()
+                
+        
+        self.text = QLabel('Ваш заказ', self)
+        self.text.setText('Ваш заказ:')
+        self.text.setStyleSheet("border: 0px")
+        self.cheque_box_layout.addWidget(self.text)
 
-        for check_box, input_box in self.input_boxes.items():
+        total_price = 0
+        has_order = False
+
+        for check_box, (input_box, price) in self.input_boxes.items():
             if check_box.isChecked():
-                self.goods.setText(f"{check_box.text()}")
-                print(check_box.text())
-            else:
-                print('0')
+                self.cheque_box_widget.setStyleSheet("border: 2px solid grey; padding: 10px;") 
+                quantity = int(input_box.text())
+                if quantity > 0:
+                    # Добавление информации о выбранном блюде
+                    order_info = QLabel(f"{check_box.text()}: {quantity} x {price} руб. = {quantity * int(price)} руб", self)
+                    order_info.setStyleSheet("border: 0px")
+                    self.cheque_box_layout.addWidget(order_info)  # Добавление в чек
+                    total_price += quantity * int(price)
+                    has_order = True  # Устанавливаем флаг, если есть 
+        if has_order:
+            total_label = QLabel(f"Итого: {total_price} руб.", self)
+            self.cheque_box_layout.addWidget(total_label)
+            self.cheque_box_widget.show()  # Показываем чек, если есть выбранные блюда
+        else:
+            self.cheque_box_widget.hide()  # Скрываем чек, если ничего не выбрано
+                
+        self.cheque_box_layout.addStretch()
+        
 
-        
-        
-        
+
+
     def count(self):
-        for check_box, input_box in self.input_boxes.items():
+        for check_box, (input_box, price) in self.input_boxes.items():
             if check_box.isChecked():
                 input_box.setDisabled(False)
                 input_box.setText("1")

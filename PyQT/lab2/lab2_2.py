21c9c9cb387420e131bbdf58ff12db8c1ca59fde
GekK9 <110110744+GekK9@users.noreply.github.com>
2025-04-01 02:21:27 +0800

    completed lab2_2

--- PyQT/lab2/lab2_2.py
+++ PyQT/lab2/lab2_2.py
@@ -1,13 +1,18 @@
 import sys
-import math
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLCDNumber
-
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('lab2_2.ui', self)
+        self.pushButton_add.clicked.connect(self.add_contact)
+        
+    def add_contact(self):
+        self.name = self.lineEdit_name.text()
+        self.phone = self.lineEdit_phone.text()
+        self.listWidget.addItem(self.name + ' ' + self.phone)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
